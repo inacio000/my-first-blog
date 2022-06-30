ef636f6c7f2dfb3012784b85646714740993eac4
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2022-06-30 22:19:26 +0300

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -9,7 +9,12 @@ name='post_list', é o nome da URL que será usado para identificar a view
 from django.urls import path
 from . import views
 
+# A parte post/<int:pk>/ especifica um padrão de URL
+# post/ significa apenas que a URL deve começar com a palavra post seguida por /
+# <int:pk> – Ela nos diz que o Django espera um objeto do tipo inteiro e que vai transferi-lo para a view como uma variável chamada pk
+# / – por fim, precisamos adicionar uma / ao final da nossa URL
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
